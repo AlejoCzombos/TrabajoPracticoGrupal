0a40db59b65e9b4415b63ef453665993d91fa499
Alejo <alejoczombos@gmail.com>
2022-09-22 20:45:40 -0300

    Clases Materia, Persona y Estudiante

--- src/TrabajoPractico_LaFacultad/Persona.java
+++ src/TrabajoPractico_LaFacultad/Persona.java
@@ -12,6 +12,42 @@ public abstract class Persona {
         this.legajo = legajo;
     }
 
-    void modificarDatos(String nombre, String apellido, int legajo){}
+    void modificarDatos(String nombre, String apellido, int legajo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.legajo = legajo;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", legajo=" + legajo +
+                "}";
+    }
 }
